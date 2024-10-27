e2a10d7e331819a8232453a47048936af8ea0619
bambovijs <raivo@bambis.lv>
2024-10-27 15:54:01 +0200

    Add EmployeeMapper for DTO to Entity conversion
    - Created `EmployeeMapper` class
    - Implemented `toEmployeeDto` method to convert `Employee` entity to `EmployeeDto`
    - Implemented `toEmployee` method to convert `EmployeeDto` to `Employee` entity

---
+++ src/main/java/com/example/demo/mapper/EmployeeMapper.java
@@ -0,0 +1,14 @@
+package com.example.demo.mapper;
+
+import com.example.demo.dto.EmployeeDto;
+import com.example.demo.enitity.Employee;
+
+public class EmployeeMapper {
+    public static EmployeeDto toEmployeeDto(Employee employee) {
+        return new EmployeeDto(employee.getId(), employee.getFirstName(), employee.getLastName(), employee.getEmail());
+    }
+
+    public static Employee toEmployee(EmployeeDto employeeDto) {
+        return new Employee(employeeDto.getId(), employeeDto.getFirstName(), employeeDto.getLastName(), employeeDto.getEmail());
+    }
+}
