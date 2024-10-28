f0e01c7aaa71b00555dd6c62da6db3e6bc4bbb80
bambovijs <raivo@bambis.lv>
2024-10-28 16:00:08 +0200

    refactor: Reorganize frontend folder structure
    - Moved and reorganized files within the `frontend` directory
    - Updated import paths and configurations to reflect new structure
    - Ensured build and development processes are unaffected by changes
    - Built components: `App`, `Header`, `Footer`, `EmployeeList`, `EmployeeForm`
    - Used `axios` for making HTTP requests to the backend API
    - Connected frontend with Bootstrap for styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import HeaderComponent from './components/HeaderComponent'
+import FooterComponent from './components/FooterComponent'
+import ListEmployeeComponent from './components/ListEmployeeComponent'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import EmployeeComponent from './components/EmployeeComponent'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <HeaderComponent />
+          <Routes>
+            <Route path='/' element={<ListEmployeeComponent />} />
+            <Route path='/employees' element={<ListEmployeeComponent />} /> 
+            <Route path='/add-employee' element={<EmployeeComponent />} />
+          </Routes>
+        <FooterComponent />
+      </BrowserRouter>
+      
     </>
   )
 }
---
+++ frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className="footer">
+            <p>All right reserved 2024 by RaBa</p>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
