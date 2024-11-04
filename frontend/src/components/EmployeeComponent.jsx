4d578464f8fe6dcf400d648966fcdd8556e99df0
bambovijs <raivo@bambis.lv>
2024-11-04 11:18:56 +0200

    Add form validation and update functionality in EmployeeComponent
    - Added form validation for first name, last name, and email fields
    - Implemented update functionality for existing employees
    - Displayed validation error messages for invalid inputs
    - Updated `EmployeeComponent` to handle both add and update operations
    - Refactored `EmployeeService` to include `getEmployee` and `updateEmployee` functions

--- frontend/src/components/EmployeeComponent.jsx
+++ frontend/src/components/EmployeeComponent.jsx
@@ -1,5 +1,7 @@
-import { func } from 'prop-types'
-import React, { useState } from 'react'
+
+import { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { createEmployee, getEmployee, updateEmployee } from '../services/EmployeeService'
 
 const EmployeeComponent = () => {
   
@@ -7,11 +9,93 @@ const EmployeeComponent = () => {
   const [lastName, setLastName] = useState('')
   const [email, setEmail] = useState('')
 
-  function saveEmployee(e) {
+  const {id} = useParams();
+
+  const [errors, setErrors] = useState({
+    firstName: '',
+    lastName: '',
+    email: ''
+  })
+
+  const navigator = useNavigate();
+
+  useEffect(() => {
+    if(id){
+      getEmployee(id).then((response) => {
+        setFirstName(response.data.firstName);
+        setLastName(response.data.lastName);
+        setEmail(response.data.email);
+      }).catch(error => {
+        console.error(error);
+      })
+    }
+  }, [id])
+
+  function saveOrUpdateEmployee(e) {
     e.preventDefault();
 
-    let employee = {firstName: firstName, lastName: lastName, email: email};
-    console.log(employee);
+    if(validateForm()){
+      let employee = {firstName: firstName, lastName: lastName, email: email};
+      console.log(employee);
+      
+      if(id){
+        updateEmployee(id, employee).then((response) => {
+          console.log(response.data);
+          navigator('/employees');
+        }).catch(error => {
+          console.error(error);
+        })
+      } else{
+        createEmployee(employee).then((response) => {
+          console.log(response.data);
+          navigator('/employees');
+        }).catch(error => {
+        console.error(error);
+        })
+      }
+    }
+  }
+
+  function validateForm(){
+    let valid = true;
+
+    const errorsCopy = {... errors};
+
+    if(firstName.trim()){
+      errorsCopy.firstName = '';
+    }
+    else{
+      errorsCopy.firstName = 'First Name is required';
+      valid = false;
+    }
+
+    if(lastName.trim()){
+      errorsCopy.lastName = '';
+    }
+    else{
+      errorsCopy.lastName = 'Last Name is required';
+      valid = false;
+    }
+
+    if(email.trim()){
+      errorsCopy.email = '';
+    }
+    else{
+      errorsCopy.email = 'Email is required';
+      valid = false;
+    }
+
+    setErrors(errorsCopy);
+
+    return valid;
+  }
+
+  function pageTitle(){
+    if(id){
+      return <h2 className='text-center mt-2 mb-2'>Update Employee</h2>
+    }else{
+      return <h2 className='text-center mt-2 mb-2'>Add Employee</h2>
+    }
   }
 
   return (
@@ -19,22 +103,29 @@ const EmployeeComponent = () => {
         <br/><br/>
         <div className='row'>
           <div className='card col-md-6 offset-md-3 offset-md-3'>
-            <h2 className='text-center mt-2 mb-2'>Add Employee</h2>
-            <form className='mb-2'>
-              <div className='form-group mb-2'>
-                <label>First Name:</label>
-                <input placeholder='First Name' name='firstName' className='form-control' value={firstName} onChange={(e) => setFirstName(e.target.value)} />
-              </div>
-              <div className='form-group mb-2'>
-                <label>Last Name:</label>
-                <input placeholder='Last Name' name='lastName' className='form-control' value={lastName} onChange={(e) => setLastName(e.target.value)} />
-              </div>
-              <div className='form-group mb-2'>
-                <label>Email:</label>
-                <input placeholder='Email' name='email' className='form-control' value={email} onChange={(e) => setEmail(e.target.value)} />
-              </div>
-              <button className='btn btn-success' onClick={saveEmployee}>Save</button>
-            </form>
+            {
+              pageTitle()
+            }
+            <div className='card-body'>
+              <form>
+                <div className='form-group mb-2'>
+                  <label>First Name:</label>
+                  <input placeholder='First Name' name='firstName' className={`form-control ${errors.firstName ? 'is-invalid': ''}`} value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+                  {errors.firstName && <div className='invalid-feedback'>{errors.firstName}</div>}
+                </div>
+                <div className='form-group mb-2'>
+                  <label>Last Name:</label>
+                  <input placeholder='Last Name' name='lastName' className={`form-control ${errors.lastName ? 'is-invalid': ''}`} value={lastName} onChange={(e) => setLastName(e.target.value)} />
+                  {errors.lastName && <div className='invalid-feedback'>{errors.lastName}</div>}
+                </div>
+                <div className='form-group mb-2'>
+                  <label>Email:</label>
+                  <input placeholder='Email' name='email' className={`form-control ${errors.email ? 'is-invalid': ''}`} value={email} onChange={(e) => setEmail(e.target.value)} />
+                  {errors.email && <div className='invalid-feedback'>{errors.email}</div>}
+                </div>
+                <button className='btn btn-success' onClick={saveOrUpdateEmployee}>Save</button>
+              </form>
+            </div>
           </div>
         </div>
     </div>
